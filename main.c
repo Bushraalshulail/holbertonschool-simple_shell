cde497321b16e382c52e7811963b2196dcdd279d
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-04-14 16:00:34 +0000

    Write Betty compliant initial shell files

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - Starts the simple shell
+* @argc: Number of arguments passed to the program
+* @argv: Array of strings containing the arguments
+*
+* Description: This is the entry point of the shell program.
+*              Currently, it does not perform any shell logic.
+*
+* Return: 0 on successful execution
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+(void)argv;
+
+/* Shell logic will go here later */
+
+return (0);
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - Entry point of the shell
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv);
+
+#endif /* MAIN_H */
