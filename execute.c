1ce055b9f64f22b579d396ded941a73b9f0d1146
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-04-19 03:19:35 +0000

    _

--- execute.c
+++ execute.c
@@ -1,75 +1,42 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * handle_execution_errors - Handle errors if execve fails.
- * @full_path: The resolved path to the command.
- * @argv: Argument list.
- */
-void handle_execution_errors(char *full_path, char **argv)
+* execute - Executes a command using execve
+* @args: Arguments passed (command + args)
+* @env: Environment variables
+* Return: status code
+*/
+int execute(char **args, char **env)
 {
-	perror(argv[0]);
-	free(full_path);
-	free(argv);
-	exit(EXIT_FAILURE);
+pid_t pid;
+int status;
+char *full_path;
+
+full_path = find_path(args[0], env);
+if (!full_path)
+{
+print_error(args[0]);
+return (127);
 }
 
-/**
- * run_child_process - Fork and execute the command.
- * @full_path: Resolved full path.
- * @argv: Argument list.
- * @env: Environment variables.
- */
-void run_child_process(char *full_path, char **argv, char **env)
+pid = fork();
+if (pid == -1)
 {
-	pid_t pid = fork();
-
-	if (pid == 0)
-	{
-		if (execve(full_path, argv, env) == -1)
-			handle_execution_errors(full_path, argv);
-	}
-	else if (pid > 0)
-	{
-		int status;
-
-		waitpid(pid, &status, 0);
-	}
-	else
-	{
-		perror("fork");
-	}
-	free(full_path);
-	free(argv);
+perror("fork");
+free(full_path);
+return (1);
+}
+else if (pid == 0)
+{
+execve(full_path, args, env);
+perror(args[0]);
+exit(127);
+}
+else
+{
+waitpid(pid, &status, 0);
+free(full_path);
 }
 
-/**
- * run_command - Resolve the command path and execute it.
- * @cmd: Command entered by user.
- * @env: Environment variables.
- */
-void run_command(char *cmd, char **env)
-{
-	char **argv = split_string(cmd);
-	char *full_path;
-
-	if (argv == NULL || argv[0] == NULL)
-	{
-		free(argv);
-		return;
-	}
-
-	if (_strcmp(argv[0], "exit") == 0)
-	{
-		free(argv);
-		exit(EXIT_SUCCESS);
-	}
-
-	full_path = resolve_command_path(argv[0], env);
-	if (full_path == NULL)
-	{
-		perror(argv[0]);
-		free(argv);
-		return;
-	}
-	run_child_process(full_path, argv, env);
+return (WEXITSTATUS(status));
 }
--- parse_line.c
+++ parse_line.c
@@ -8,33 +8,33 @@
  */
 char **parse_line(char *line)
 {
-int bufsize = 64, i = 0;
-char **tokens = malloc(bufsize * sizeof(char *));
-char *token;
+	int bufsize = 64, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-if (!tokens)
-{
-perror("malloc");
-exit(EXIT_FAILURE);
-}
+	if (!tokens)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-token = strtok(line, " \n\t");
-while (token)
-{
-tokens[i++] = token;
-if (i >= bufsize)
-{
-bufsize += 64;
-tokens = realloc(tokens, bufsize *sizeof(char *));
-if (!tokens)
-{
-perror("realloc");
-exit(EXIT_FAILURE);
-}
-}
-token = strtok(NULL, " \n\t");
-}
-tokens[i] = NULL;
-return (tokens);
-free(tokens);
+	token = strtok(line, " \n\t");
+	while (token)
+	{
+		tokens[i++] = token;
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, " \n\t");
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
 }
